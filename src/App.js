c76e34d5efa634da86ffc8c258531bbd0993cc11
varunkarwa <66462469+varunkarwa@users.noreply.github.com>
2020-12-15 19:37:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React ,{Fragment} from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component{
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({loading: true});
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false});
+  }
+
+  render()
+  {
+    return (
+    <Fragment className="App">
+      <Navbar />
+      <div className='container'>
+        <Users loading={this.state.loading} users={this.state.users}/>
+      </div>
+    </Fragment>
+    );
+  }
+}
+
+export default App;
